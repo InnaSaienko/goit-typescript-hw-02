b9275308b07a4daf554aaf31a0ac7691300403f0
Inna Saienko <inna.saienko.nl@gmail.com>
2025-05-19 12:10:28 +0200

    refactor to tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import Modal from "react-modal";
+import {Toaster} from "react-hot-toast";
+
+Modal.setAppElement('#root');
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <App />
+        <Toaster/>
+    </StrictMode>,
 )
