300cc21c9f37f1ae6b58cd471f625cfce2b5aeb5
Inna Saienko <inna.saienko.nl@gmail.com>
2025-05-19 15:22:00 +0200

    convert the page and perPage parameters to strings before setting them in the URLSearchParams

--- src/hooks/api.ts
+++ src/hooks/api.ts
@@ -3,15 +3,15 @@ import axios from 'axios';
 const accessKey = import.meta.env.VITE_UNSPLASH_API_KEY;
 const pathSearch = "/search/photos";
 
-export const useFetchPhotos = async ({query, page, photosPerPage, signal}: FetchPhotoParams): Promise<object[]> => {
+export const useFetchPhotos = async ({query, page, perPage, signal}: FetchPhotoParams): Promise<object[]> => {
     const url = new URL(pathSearch, 'https://api.unsplash.com');
 
     if (query) {
         url.searchParams.set("orientation", "landscape");
         url.searchParams.set("query", query);
     }
-    url.searchParams.set("per_page", photosPerPage);
-    url.searchParams.set("page", page);
+    url.searchParams.set("per_page",perPage.toString());
+    url.searchParams.set("page", page.toString());
     const response = await axios.get(url.toString(), {
         headers: {
             Authorization: `Client-ID ${accessKey}`,
