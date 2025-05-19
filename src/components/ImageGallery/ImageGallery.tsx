24197beb005dfaf16d3159d333d6edb6c9b83296
Inna Saienko <inna.saienko.nl@gmail.com>
2025-05-19 12:08:29 +0200

    refactor to tsx

---
+++ src/components/ImageGallery/ImageGallery.tsx
@@ -0,0 +1,24 @@
+import ImageCard from "../ImageCard/ImageCard.jsx";
+import s from "./ImageGallery.module.css"
+
+interface ImageGalleryProps {
+    photos: Photo[];
+    onSelect: (photo: Photo) => void;
+}
+const ImageGallery = ({photos, onSelect} : ImageGalleryProps ) => {
+    return (
+            <>
+                <ul className={s.gallery}>
+                    {photos.map((photo, index) => (
+                        <ImageCard
+                            key={index}
+                            photo={photo}
+                            onClick={() => onSelect(photo)}
+                        />
+                    ))}
+                </ul>
+        </>
+    )
+}
+
+export default ImageGallery;
